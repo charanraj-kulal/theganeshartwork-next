3f9beaa4e65dc27a8162a403f969c5d0f7a38b2e
charanrraj-kulal <c191542709@gmail.com>
2025-12-18 22:13:59 +0530

    Update build script to generate Prisma client before build

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['decorior.in'],
   },
-};
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
+    },
+  },
+}
 
 export default nextConfig;
